2a930814854533015fa0f962eafb35760b77cd48
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-07-18 12:18:00 +0300

    Create prototypehain.js

---
+++ prototypehain.js
@@ -0,0 +1,13 @@
+consy animal = {
+  eats:true
+};
+
+const animal = Object.create(animal);
+mamal.hasfur = true;
+
+const dog = Object.create(mamal);
+dog.barks = true;
+
+console.log(dogs.eats);
+console.log(dog.hasfur);
+console.log(dog.barks);
